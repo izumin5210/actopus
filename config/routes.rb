70e483321dceeb2a0c79792f908fb9582d97bb0b
やましー <3.1415mailadress@gmail.com>
2014-12-23 16:14:20 +0900

    Add staff/home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :staffs
+  
+  namespace :staffs do
+    get 'home' => 'home#home'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
