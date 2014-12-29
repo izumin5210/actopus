13fb913c9f716535edf93f6aaec8d6c9c4d69d4e
izumin5210 <masayuki@izumin.info>
2014-12-30 01:07:56 +0900

    Refactor TimetableForm#save

--- app/models/timetable_form.rb
+++ app/models/timetable_form.rb
@@ -14,33 +14,51 @@ class TimetableForm
     Term.transaction do
       @term = Term.create!(name: @name, start_on: @start_on, end_on: @end_on,
           xml_filename: @timetable_xml.original_filename)
-      departments = Department.all
-      courses = Course.all
-      periods = Period.all
-      lecturers = Lecturer.all
-      Lecture.transaction do
-        LectureMapper.parse(@timetable_xml.tempfile.read).each do |mapper|
-          lecture = mapper.to_record
-          lecture.department = departments.find { |d| d.name == mapper.department }
-          lecture.course = courses.find { |d| d.name == mapper.course }
-          lecture.term = term
-          lecture.save!
-          mapper.period_params.each do |params|
-            period = periods.find do |p|
-                (p.start_time == params[:start_time]) && (p.end_time == params[:end_time])
-              end
-            lecture.held_lectures.create!(period: period, wday: mapper.wday)
-          end
-          mapper.lecturer_params.each do |params|
-            lecturer = lecturers.find { |l| l.name == params[:name] }
-            if lecturer.blank?
-              lecturer = Lecturer.create!(name: params[:name])
-              lecturers << lecturer
-            end
-            lecture.lecturers << lecturer
-          end
-        end
+      lecture_transaction
+    end
+  end
+
+  private
+
+  def lecture_transaction
+    # TODO: 発行されるクエリ数が多すぎるのでバルクインサート等の対策を検討
+    departments, courses = Department.all, Course.all
+    periods, lecturers = Period.all, Lecturer.all
+    Lecture.transaction do
+      LectureMapper.parse(@timetable_xml.tempfile.read).each do |mapper|
+        lecture = create_lecture(mapper, departments, courses)
+        map_periods_onto_lecture(mapper, lecture, periods)
+        map_lecturers_onto_lecture(mapper, lecture, lecturers)
       end
+    end
+  end
+
+  def create_lecture(mapper, departments, courses)
+    mapper.to_record.tap do |lecture|
+      lecture.department = departments.find { |d| d.name == mapper.department }
+      lecture.course = courses.find { |d| d.name == mapper.course }
+      lecture.term = @term
+      lecture.save!
+    end
+  end
+
+  def map_periods_onto_lecture(mapper, lecture, periods)
+    mapper.period_params.each do |params|
+      period = periods.find do |p|
+        (p.start_time == params[:start_time]) && (p.end_time == params[:end_time])
+      end
+      lecture.held_lectures.create!(period: period, wday: mapper.wday)
+    end
+  end
+
+  def map_lecturers_onto_lecture(mapper, lecture, lecturers)
+    mapper.lecturer_params.each do |params|
+      lecturer = lecturers.find { |l| l.name == params[:name] }
+      if lecturer.blank?
+        lecturer = Lecturer.create!(name: params[:name])
+        lecturers << lecturer
+      end
+      lecture.lecturers << lecturer
     end
   end
 end
