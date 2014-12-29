ee8a58732383a20dd01d24321421e38416828af8
izumin5210 <masayuki@izumin.info>
2014-12-30 00:24:48 +0900

    Implement staffs/timetables#create action

--- app/controllers/staffs/timetables_controller.rb
+++ app/controllers/staffs/timetables_controller.rb
@@ -4,6 +4,15 @@ class Staffs::TimetablesController < Staffs::BaseController
   end
 
   def create
+    @form = TimetableForm.new(timetable_params)
+    @form.save
     redirect_to new_staffs_timetable_path, notice: 'Not yet implemented.'
+  end
+
+  private
+
+  def timetable_params
+    params.require(:timetable_form)
+      .permit(:name, :start_on, :end_on, :timetable_xml)
   end
 end
