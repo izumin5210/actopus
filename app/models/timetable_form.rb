7415a53c6c167894a6e683516b896dba20d8b704
izumin5210 <masayuki@izumin.info>
2014-12-30 00:24:17 +0900

    Implement TimetableForm#save

--- app/models/timetable_form.rb
+++ app/models/timetable_form.rb
@@ -1,14 +1,46 @@
 class TimetableForm
   include ActiveModel::Model
 
-  attr_reader :name, :start_on, :end_on, :timetable_xml
+  attr_reader :term
+  attr_accessor :name, :start_on, :end_on, :timetable_xml
 
-  def initialize(*args)
-    args.each do |attr, value|
+  def initialize(attrs = {})
+    attrs.each do |attr, value|
       public_send("#{attr}=", value) if respond_to?("#{attr}=")
     end
   end
 
   def save
+    Term.transaction do
+      @term = Term.create!(name: @name, start_on: @start_on, end_on: @end_on,
+          xml_filename: @timetable_xml.original_filename)
+      departments = Department.all
+      courses = Course.all
+      periods = Period.all
+      lecturers = Lecturer.all
+      Lecture.transaction do
+        LectureMapper.parse(@timetable_xml.tempfile.read).each do |mapper|
+          lecture = mapper.to_record
+          lecture.department = departments.find { |d| d.name == mapper.department }
+          lecture.course = courses.find { |d| d.name == mapper.course }
+          lecture.term = term
+          lecture.save!
+          mapper.period_params.each do |params|
+            period = periods.find do |p|
+                (p.start_time == params[:start_time]) && (p.end_time == params[:end_time])
+              end
+            lecture.held_lectures.create!(period: period, wday: mapper.wday)
+          end
+          mapper.lecturer_params.each do |params|
+            lecturer = lecturers.find { |l| l.name == params[:name] }
+            if lecturer.blank?
+              lecturer = Lecturer.create!(name: params[:name])
+              lecturers << lecturer
+            end
+            lecture.lecturers << lecturer
+          end
+        end
+      end
+    end
   end
 end
---
+++ spec/models/timetable_form_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe TimetableForm do
+  let(:timetable_form) do
+    TimetableForm.new(
+      name: '後期', timetable_xml: xml,
+      start_on: Date.new(2014, 10, 1), end_on: Date.new(2015, 2, 28))
+  end
+
+  let(:xml) do
+    double('timetable').tap do |d|
+      allow(d).to receive_message_chain(:tempfile, :read).and_return(timetable_xml(lecture_xmls))
+      allow(d).to receive(:original_filename).and_return('timetable201410.xml')
+    end
+  end
+
+  before do
+    %w(department course period).each do |seed|
+      load Rails.root.join("db/seeds/#{seed}.rb")
+    end
+  end
+
+  let(:lecture_xmls) do
+    [
+      lecture_xml,
+      lecture_xml(
+        name: '機械工学実習 II', grade: 2, department: '機械工学科', wday: 2,
+        lecturers: ['加藤 隆弘', '関森 大介', '岩野 優樹', '大森 茂俊'],
+        periods: [
+          { start_time: '09:00:00+09:00', end_time: '10:30:00+09:00' },
+          { start_time: '10:40:00+09:00', end_time: '12:10:00+09:00' }
+        ]
+      )
+    ]
+  end
+
+  describe '#save' do
+    subject { timetable_form.save }
+    it { expect { subject }.to change(Term, :count).by(1) }
+    it { expect { subject }.to change(Lecture, :count).by(2) }
+    it { expect { subject }.to change(Lecturer, :count).by(5) }
+    it do
+      department = Department.find_by(name: '電気情報工学科')
+      expect { subject }.to change(department.lectures, :count).by(1)
+    end
+    it do
+      period = Period.find_by(start_time: '09:00:00+09:00', end_time: '10:30:00+09:00')
+      expect { subject }.to change(period.lectures, :count).by(2)
+    end
+    it { expect(timetable_form.tap(&:save).term.lectures.size).to eq 2 }
+  end
+end
