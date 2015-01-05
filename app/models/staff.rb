2124aaf5152e7b6a2b74b91a88919bd2ee2dcbda
やましー <3.1415mailadress@gmail.com>
2015-01-05 20:42:21 +0900

    Set to send mails

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -30,6 +30,6 @@
 class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
