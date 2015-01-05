7c3c783ef9df6849a62348978b1980fe9d78e45d
やましー <3.1415mailadress@gmail.com>
2015-01-05 15:48:30 +0900

    Improve sign_up's view

--- app/controllers/staffs/sessions_controller.rb
+++ app/controllers/staffs/sessions_controller.rb
@@ -1,4 +1,6 @@
-class Staffs::SessionsController < Staffs::BaseController
+class Staffs::SessionsController < Devise::SessionsController
+  layout 'staffs'
+
   def after_sign_in_path_for(resource)
     staffs_home_path
   end
