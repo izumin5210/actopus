5e5e9a48fee81f8cc6d6d05e7998031cb6a79f06
izumin5210 <masayuki@izumin.info>
2015-01-11 15:49:18 +0900

    Add staffs/lecturers#timetable

--- app/controllers/staffs/lecturers_controller.rb
+++ app/controllers/staffs/lecturers_controller.rb
@@ -1,5 +1,17 @@
 class Staffs::LecturersController < Staffs::BaseController
+  before_action :set_lecturer, only: %i(timetable)
+
   def index
     @lecturers = Lecturer.all
+  end
+
+  def timetable
+    @timeline = LectureTimeline.new(@lecturer.lectures)
+  end
+
+  private
+
+  def set_lecturer
+    @lecturer = Lecturer.find(params[:id])
   end
 end
