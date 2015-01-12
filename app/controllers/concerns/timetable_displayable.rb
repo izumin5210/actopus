c7b437741dbb67a1ef57c74634d6591d4cf444e2
izumin5210 <masayuki@izumin.info>
2015-01-12 17:43:16 +0900

    Support weekly timetable

--- app/controllers/concerns/timetable_displayable.rb
+++ app/controllers/concerns/timetable_displayable.rb
@@ -3,6 +3,8 @@ module TimetableDisplayable
 
   included do
     before_action :set_date, only: %i(timetable)
+    before_action :set_beginning_of_week, only: %i(timetable)
+    before_action :set_end_of_week, only: %i(timetable)
     before_action :set_date_step, only: %i(timetable)
     before_action :set_lectures, only: %i(timetable)
   end
@@ -13,13 +15,21 @@ module TimetableDisplayable
 
   private
 
-  def set_date_step
-    @date_step = @date.beginning_of_week.step(@date.end_of_week)
-  end
-
   def set_date
     @date = Date.parse(params[:date])
   rescue
     @date = Date.current
+  end
+
+  def set_beginning_of_week
+    @beginning_of_week = @date.beginning_of_week
+  end
+
+  def set_end_of_week
+    @end_of_week = @date.end_of_week
+  end
+
+  def set_date_step
+    @date_step = @beginning_of_week.step(@end_of_week)
   end
 end
