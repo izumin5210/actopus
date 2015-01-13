901caf58a70e8a54cbbb4d6c9a4ea074fac0fe6f
izumin5210 <masayuki@izumin.info>
2015-01-13 15:03:29 +0900

    Enable invitable module to staff

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -39,5 +39,6 @@ class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :invitable, invite_for: 2.weeks
 end
