15310d1f69a9cc91203350921ef78e0968581d9e
izumin5210 <masayuki@izumin.info>
2015-01-13 16:19:34 +0900

    Remove confirmable module from Staff model

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -33,7 +33,7 @@
 class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :invitable, invite_for: 2.weeks
 end
