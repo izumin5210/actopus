6cdc560b77e91e323bbd65217c9c838835102ea2
izumin5210 <masayuki@izumin.info>
2015-01-14 19:13:46 +0900

    Add ReschedulingForm

---
+++ app/models/rescheduling_form.rb
@@ -0,0 +1,25 @@
+class ReschedulingForm
+  include ActiveModel::Model
+
+  attr_accessor :lecture_id, :category,
+    :before_period_id, :before_date,
+    :after_period_id, :after_period_id
+
+  validates :lecture_id, presence: true
+  validates :category, presence: true
+  validates :before_period_id, presence: true
+  validates :before_date, presence: true
+
+  def save
+    ActiveRecord::Base.transaction do
+      before_dp_params = { period_id: before_period_id, taken_on: before_date }
+      before_dp = DatePeriod.find_or_create_by!(before_dp_params)
+      Rescheduling.create!(
+        category: category,
+        before_date_period_id: before_dp.id, lecture_id: lecture_id)
+      true
+    end
+  rescue
+    false
+  end
+end
---
+++ spec/models/rescheduling_form_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe ReschedulingForm, type: :model do
+  let(:rescheduling_form) { ReschedulingForm.new(params) }
+  let(:params) { {} }
+  let(:lecture) { create(:lecture, :with_term, :with_klass) }
+  let(:period) { create(:period) }
+  subject { rescheduling_form }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:lecture_id) }
+    it { is_expected.to validate_presence_of(:category) }
+    it { is_expected.to validate_presence_of(:before_period_id) }
+    it { is_expected.to validate_presence_of(:before_date) }
+  end
+
+  describe '#save' do
+    subject { rescheduling_form.save }
+    context 'with invalid params' do
+      let(:params) do
+        { category: 'cancel',
+          before_period_id: period.id, before_date: Date.parse('2015-01-14') }
+      end
+      it { is_expected.to be false }
+      it { expect { subject }.to_not change(lecture.reschedulings, :count) }
+      it { expect { subject }.to_not change(Rescheduling, :count) }
+      it { expect { subject }.to_not change(DatePeriod, :count) }
+    end
+    context 'when category is cancel' do
+      let(:params) do
+        { lecture_id: lecture.id, category: 'cancel',
+          before_period_id: period.id, before_date: Date.parse('2015-01-14') }
+      end
+      it { is_expected.to be true }
+      it { expect { subject }.to change(lecture.reschedulings, :count).by(1) }
+      it { expect { subject }.to change(Rescheduling, :count).by(1) }
+      it { expect { subject }.to change(DatePeriod, :count).by(1) }
+    end
+  end
+end
