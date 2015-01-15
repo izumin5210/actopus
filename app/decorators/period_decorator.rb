39a92e401e1565954c87082f88cd18b45d49bc0d
izumin5210 <masayuki@izumin.info>
2015-01-15 12:45:17 +0900

    Add PeriodDecorator and implement #to_display

---
+++ app/decorators/period_decorator.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+module PeriodDecorator
+  def to_display
+    time_str = [start_time, end_time].map { |t|
+        Time.parse(t).strftime('%H:%M')
+      }.join(' ~ ')
+    "#{name} #{time_str}"
+  end
+end
