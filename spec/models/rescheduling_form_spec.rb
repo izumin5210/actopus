7a567b280871db64f2277dc8f5588cb3cc847eab
izumin5210 <masayuki@izumin.info>
2015-01-16 09:09:13 +0900

    Refactor ReschedulingForm spec

--- spec/models/rescheduling_form_spec.rb
+++ spec/models/rescheduling_form_spec.rb
@@ -2,10 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ReschedulingForm, type: :model do
   let(:rescheduling_form) { ReschedulingForm.new(params) }
-  let(:params) { {} }
   let(:lecture) { create(:lecture, :with_term, :with_klass) }
   let(:period) { create(:period) }
   subject { rescheduling_form }
+
+  let(:params) do
+    { lecture_id: lecture.id, category: category,
+      before_period_id: before_period.try(:id), before_date: before_date,
+      after_period_id: after_period.try(:id), after_date: after_date
+    }
+  end
+  let(:category) { 'cancel' }
+  let(:before_period) { period }
+  let(:after_period) { create(:period) }
+  let(:before_date) { Date.parse('2015-01-14') }
+  let(:after_date) { Date.parse('2015-01-22') }
+
+  shared_examples_for 'a invalid rescheduling form' do
+    it { is_expected.to be false }
+    it { expect { subject }.to_not change(lecture.reschedulings, :count) }
+    it { expect { subject }.to_not change(Rescheduling, :count) }
+    it { expect { subject }.to_not change(DatePeriod, :count) }
+  end
+
+  shared_examples_for 'a valid rescheduling form' do
+    it { is_expected.to be true }
+    it { expect { subject }.to change(lecture.reschedulings, :count).by(1) }
+    it { expect { subject }.to change(Rescheduling, :count).by(1) }
+    it { expect { subject }.to change(DatePeriod, :count).by(1) }
+  end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:category) }
@@ -13,25 +38,17 @@ RSpec.describe ReschedulingForm, type: :model do
 
   describe '#save' do
     subject { rescheduling_form.save }
-    context 'with invalid params' do
-      let(:params) do
-        { category: 'cancel',
-          before_period_id: period.id, before_date: Date.parse('2015-01-14') }
-      end
-      it { is_expected.to be false }
-      it { expect { subject }.to_not change(lecture.reschedulings, :count) }
-      it { expect { subject }.to_not change(Rescheduling, :count) }
-      it { expect { subject }.to_not change(DatePeriod, :count) }
+    it_behaves_like 'a invalid rescheduling form' do
+      let(:category) { 'pyonpyon' }
     end
-    context 'when category is cancel' do
-      let(:params) do
-        { lecture_id: lecture.id, category: 'cancel',
-          before_period_id: period.id, before_date: Date.parse('2015-01-14') }
+    context 'when category is "cancel"' do
+      let(:category) { 'cancel' }
+      let(:after_period) { nil }
+      let(:after_date) { nil }
+      it_behaves_like 'a invalid rescheduling form' do
+        let(:before_period) { nil }
       end
-      it { is_expected.to be true }
-      it { expect { subject }.to change(lecture.reschedulings, :count).by(1) }
-      it { expect { subject }.to change(Rescheduling, :count).by(1) }
-      it { expect { subject }.to change(DatePeriod, :count).by(1) }
+      it_behaves_like 'a valid rescheduling form'
     end
   end
 end
