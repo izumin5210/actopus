4e26fa2289881e6df1950973ed9d4c96511e7e34
izumin5210 <masayuki@izumin.info>
2015-01-19 14:14:00 +0900

    Fix staffs/staffs_controller_spec

--- spec/controllers/staffs/staffs_controller_spec.rb
+++ spec/controllers/staffs/staffs_controller_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Staffs::StaffsController, :type => :controller do
+RSpec.describe Staffs::StaffsController, type: :controller do
+  sign_in_as_staff
 
-  describe "GET index" do
-    it "returns http success" do
+  describe 'GET index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'POST invite' do
+    let(:params) { { staff: attributes_for(:staff) } }
+    after { ActionMailer::Base.deliveries.clear }
+    it 'returns http found' do
+      post :invite, params
+      expect(response).to have_http_status(:found)
+    end
+    it 'saves the new staff in the database' do
+      expect { post :invite, params }.to change(Staff, :count).by(1)
+    end
+    it 'sends the invite mail to the new staff' do
+      expect {
+        post :invite, params
+      }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+  end
 end
