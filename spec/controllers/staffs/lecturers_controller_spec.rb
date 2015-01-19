2d7ded39c6b3d6e40ecdd12cb4f5a70225b9f8ca
izumin5210 <masayuki@izumin.info>
2015-01-19 13:37:03 +0900

    Fix staffs/lecturers_controller_spec

--- spec/controllers/staffs/lecturers_controller_spec.rb
+++ spec/controllers/staffs/lecturers_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe Staffs::LecturersController, :type => :controller do
+RSpec.describe Staffs::LecturersController, type: :controller do
+  sign_in_as_staff
 
-  describe "GET index" do
-    it "returns http success" do
+  let(:lecturer) { create(:lecturer) }
+
+  describe 'GET index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'PATCH update' do
+    let(:department) { create(:department) }
+    before do
+      patch :update,
+            id: lecturer.id,
+            lecturer: { department_id: department.id }
+    end
+    it 'returns http found' do
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe 'GET timetable' do
+    before { get :timetable, id: lecturer.id }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
