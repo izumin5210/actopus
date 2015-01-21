7e2b0ba429058087bfa9564a735c7389ff4e0e74
izumin5210 <masayuki@izumin.info>
2015-01-21 15:14:25 +0900

    Partialize and refactor index of reschedulings

--- app/controllers/staffs/home_controller.rb
+++ app/controllers/staffs/home_controller.rb
@@ -1,5 +1,9 @@
 class Staffs::HomeController < Staffs::BaseController
   def home
-    @reschedulings = Rescheduling.all
+    @reschedulings = Rescheduling.includes(
+        lecture: [:lecturers, :klass],
+        before_date_period: [:period],
+        after_date_period: [:period]
+      ).all
   end
 end
