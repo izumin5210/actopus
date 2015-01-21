63cdacde8f17f1d14e0c36bf85b53c33ea9b5981
izumin5210 <masayuki@izumin.info>
2015-01-21 14:42:54 +0900

    Partialize index of lecturers

--- app/controllers/staffs/reschedulings/lecturers_controller.rb
+++ app/controllers/staffs/reschedulings/lecturers_controller.rb
@@ -1,3 +1,8 @@
 class Staffs::Reschedulings::LecturersController < Staffs::LecturersController
   include ReschedulingsCreatable
+
+  def index
+    @lecturers = Lecturer.all.group_by(&:department_id)
+    @departments = Department.all
+  end
 end
