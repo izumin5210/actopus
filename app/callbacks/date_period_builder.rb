1ab3bb4cd9f9c98bd8687bf005172b5db0c17da2
izumin5210 <masayuki@izumin.info>
2015-02-09 15:34:22 +0900

    Add DatePeriodBuilder

---
+++ app/callbacks/date_period_builder.rb
@@ -0,0 +1,32 @@
+class DatePeriodBuilder
+  def initialize(period_id_column, taken_on_column, date_period_column)
+    @period_id_column = period_id_column
+    @taken_on_column = taken_on_column
+    @date_period_column = date_period_column
+  end
+
+  def before_validation(record)
+    if should_build?(record)
+      record.send(:"#{@date_period_column}=", date_period(record))
+    end
+  end
+
+  private
+
+  def should_build?(record)
+    [period_id(record), taken_on(record)].all?(&:present?)
+  end
+
+  def period_id(record)
+    record.send(@period_id_column)
+  end
+
+  def taken_on(record)
+    record.send(@taken_on_column)
+  end
+
+  def date_period(record)
+    DatePeriod.find_or_initialize_by(
+      period_id: period_id(record), taken_on: taken_on(record))
+  end
+end
