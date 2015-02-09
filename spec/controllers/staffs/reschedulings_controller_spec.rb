2181c86aa6de6ab19bdcd89a130f2239069502f9
izumin5210 <masayuki@izumin.info>
2015-02-09 18:42:35 +0900

    Fix the spec of staffs/reschedulings#new

--- spec/controllers/staffs/reschedulings_controller_spec.rb
+++ spec/controllers/staffs/reschedulings_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Staffs::ReschedulingsController, :type => :controller do
+RSpec.describe Staffs::ReschedulingsController, type: :controller do
+  sign_in_as_staff
 
-  describe "GET new" do
-    it "returns http success" do
+  describe 'GET new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
-
 end
