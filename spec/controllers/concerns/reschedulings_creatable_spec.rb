c7287d0dffa3bebb5a33880d193a430a6a36fc64
izumin5210 <masayuki@izumin.info>
2015-02-09 19:46:38 +0900

    Add specs for ReschedulingCreatable

--- spec/controllers/concerns/reschedulings_creatable_spec.rb
+++ spec/controllers/concerns/reschedulings_creatable_spec.rb
@@ -3,9 +3,26 @@ require 'rails_helper'
 RSpec.describe ReschedulingsCreatable, type: :controller do
   controller(ApplicationController) do
     include ReschedulingsCreatable
+
+    def timetable
+      render nothing: true
+    end
   end
 
-  describe 'POST create' do
-    it 'saved the new rescheduling'
+  before do
+    routes.draw { get 'timetable' => 'anonymous#timetable' }
+    get :timetable
+  end
+
+  describe 'GET timetable' do
+    it 'assigns all periods to @periods' do
+      expect(assigns(:periods)).to all(be_a Period)
+    end
+
+    context 'with no params' do
+      it 'assigns a new ReschedulingForm to @form' do
+        expect(assigns(:form)).to be_a(ReschedulingForm)
+      end
+    end
   end
 end
