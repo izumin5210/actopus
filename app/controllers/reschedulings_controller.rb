a989983462acb8fda17d96b519bd9d1ee2953e17
izumin5210 <masayuki@izumin.info>
2015-02-11 10:41:36 +0900

    Apply Rescheduling.available scope to reschedulings#index

--- app/controllers/reschedulings_controller.rb
+++ app/controllers/reschedulings_controller.rb
@@ -3,10 +3,10 @@ class ReschedulingsController < ApplicationController
   layout 'unsignedin_users'
 
   def index
-    @reschedulings = Rescheduling.includes(
+    @reschedulings = Rescheduling.available.includes(
         lecture: [:lecturers, :klass],
         before_date_period: [:period],
         after_date_period: [:period]
-      ).all
+      )
   end
 end
