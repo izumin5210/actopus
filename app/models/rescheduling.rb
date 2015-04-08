831491053189ae25041e303b3a3cf0893eb541f0
izumin5210 <masayuki@izumin.info>
2015-04-08 15:28:34 +0900

    Remove Rescheduling#before_date_period and Rescheduling#after_date_period

--- app/models/rescheduling.rb
+++ app/models/rescheduling.rb
@@ -2,19 +2,15 @@
 #
 # Table name: reschedulings
 #
-#  id                    :integer          not null, primary key
-#  lecture_id            :integer
-#  before_date_period_id :integer
-#  after_date_period_id  :integer
-#  category              :integer
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
+#  id         :integer          not null, primary key
+#  lecture_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  category   :integer
 #
 
 class Rescheduling < ActiveRecord::Base
   belongs_to :lecture
-  belongs_to :before_date_period, class_name: 'DatePeriod'
-  belongs_to :after_date_period, class_name: 'DatePeriod'
 
   validates :lecture_id, presence: true
   validates :category, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408062311) do
+ActiveRecord::Schema.define(version: 20150408062701) do
 
   create_table "academic_terms", force: :cascade do |t|
     t.string   "name",         null: false
@@ -163,10 +163,8 @@ ActiveRecord::Schema.define(version: 20150408062311) do
 
   create_table "reschedulings", force: :cascade do |t|
     t.integer  "lecture_id"
-    t.integer  "before_date_period_id"
-    t.integer  "after_date_period_id"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "category"
   end
 
