3cd65a15addf782f09ed5cf605a27d5b62f0db57
izumin5210 <masayuki@izumin.info>
2015-05-07 11:34:41 +0900

    Add write_rescheduling scope

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,9 +1,14 @@
 Garage.configure {}
+
 Garage::TokenScope.configure do
-  register :public, desc: 'acessing publicly available data' do
+  register :public, desc: 'accessing publicly available data' do
     access :read, Lecture
     access :read, Klass
     access :read, Rescheduling
+  end
+
+  register :write_rescheduling, desc: 'writing rescheduling' do
+    access :write, Rescheduling
   end
 end
 
@@ -11,4 +16,10 @@ Doorkeeper.configure do
   orm :active_record
   default_scopes :public
   optional_scopes(*Garage::TokenScope.optional_scopes)
+
+  resource_owner_authenticator do
+    current_staff || warden.authenticate!(scope: :staff)
+  end
+
+  grant_flows %w(implicit)
 end
