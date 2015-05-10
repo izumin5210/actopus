7ce00ddd1b03cee05c64e86f002da9e908a73211
izumin5210 <masayuki@izumin.info>
2015-05-11 01:44:22 +0900

    Un-ignore bundle.d.ts and add them

---
+++ spec/javascripts/bundle.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../ui/assets/typings/vendor/angularjs/angular-mocks.d.ts" />
+/// <reference path="../../ui/assets/typings/vendor/karma-jasmine/karma-jasmine.d.ts" />
+/// <reference path="../../ui/assets/typings/vendor/jasmine/jasmine.d.ts" />
+
---
+++ ui/assets/typings/bundle.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="vendor/browserify/browserify.d.ts" />
+/// <reference path="vendor/node/node.d.ts" />
+/// <reference path="vendor/angularjs/angular.d.ts" />
+/// <reference path="vendor/jquery/jquery.d.ts" />
