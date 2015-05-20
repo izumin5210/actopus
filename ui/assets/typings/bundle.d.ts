918bc727fa5fc6ecc5b45ab1523b894ef3a55ff1
izumin5210 <masayuki@izumin.info>
2015-05-21 01:44:19 +0900

    dtsm uninstall browserify

--- ui/assets/typings/bundle.d.ts
+++ ui/assets/typings/bundle.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="vendor/browserify/browserify.d.ts" />
-/// <reference path="vendor/node/node.d.ts" />
 /// <reference path="vendor/angularjs/angular.d.ts" />
 /// <reference path="vendor/jquery/jquery.d.ts" />
