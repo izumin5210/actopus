2e796bfc6a3edfa2e49902e6cbb5d1e80533f90e
izumin5210 <masayuki@izumin.info>
2015-05-21 17:33:01 +0900

    Add sample template file

--- ui/assets/javascripts/directives/hello.ts
+++ ui/assets/javascripts/directives/hello.ts
@@ -1,6 +1,6 @@
 class HelloDirective implements ng.IDirective {
   restrict: string = "E";
-  template: string = "<h1>hello world!</h1>"
+  templateUrl: string = "hello.html";
 }
 
 export = HelloDirective;
