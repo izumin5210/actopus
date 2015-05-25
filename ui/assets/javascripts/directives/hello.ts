67f1f65a71f89cba98941635e590c5f9260d7a10
izumin5210 <masayuki@izumin.info>
2015-05-25 11:53:44 +0900

    Fix sample directive

--- ui/assets/javascripts/directives/hello.ts
+++ ui/assets/javascripts/directives/hello.ts
@@ -1,6 +1,12 @@
+import angular = require("angular");
+import {appName, prefix} from "../constants";
+
 class HelloDirective implements ng.IDirective {
   restrict: string = "E";
   templateUrl: string = "hello.html";
 }
 
-export = HelloDirective;
+let app = angular.module(appName);
+app.directive(`${prefix}Hello`, [
+  () => { return new HelloDirective(); }
+]);
