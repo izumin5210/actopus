85b805e384745997fb042c1a7549008457635e80
izumin5210 <masayuki@izumin.info>
2015-05-25 11:27:07 +0900

    dtsm install angularjs/angular-route.d.ts

--- ui/assets/typings/bundle.d.ts
+++ ui/assets/typings/bundle.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="vendor/angularjs/angular.d.ts" />
 /// <reference path="vendor/jquery/jquery.d.ts" />
 /// <reference path="vendor/angularjs/angular-resource.d.ts" />
+/// <reference path="vendor/angularjs/angular-route.d.ts" />
