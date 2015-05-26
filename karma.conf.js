bb0406f1d93e1cd672aa867a417ddd57aa43afe4
izumin5210 <masayuki@izumin.info>
2015-05-26 16:05:03 +0900

    Fix karma.js.conf to use browserify and babelify

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['browserify', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
@@ -33,17 +33,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'spec/javascripts/**/**_spec.js': ['babel']
+      'spec/javascripts/**/**_spec.js': ['browserify']
     },
 
 
-    babelPreprocessor: {
-      filename: function (file) {
-        return file.originalPath.replace(/\.js$/, '.es5.js');
-      },
-      sourceFileName: function (file) {
-        return file.originalPath;
-      }
+    browserify: {
+      transform: ['babelify']
     },
 
 
