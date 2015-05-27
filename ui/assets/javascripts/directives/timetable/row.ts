8e0ae4f7414d28a6e8181b9f208e30b231da5ff2
izumin5210 <masayuki@izumin.info>
2015-05-27 13:44:51 +0900

    Add TimetableRow directive test

--- ui/assets/javascripts/directives/timetable/row.ts
+++ ui/assets/javascripts/directives/timetable/row.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../typings/vendor/angularjs/angular-resource.d.ts" />
-
 import angular = require("angular");
 import {appName, prefix} from "../../constants";
 import {TimetableResource} from "../../resources/timetable";
@@ -11,7 +9,7 @@ class TimetableRowController {
 // class TimetableRowDirective implements ng.IDirective {
 class TimetableRowDirective {
   restrict = "E";
-  controller = ["$scope", TimetableRowController];
+  controller = [TimetableRowController];
   controllerAs = "timetableRow";
   scope = {};
   bindToController = {
